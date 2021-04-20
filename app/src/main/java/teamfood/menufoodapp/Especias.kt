958ccda12875a742cc9a65e5f45394845f22d1f9
Israel Eguino <israel.eguino204244@potros.itson.edu.mx>
2021-04-20 13:57:31 -0600

    Seccion explorar

---
+++ app/src/main/java/teamfood/menufoodapp/Especias.kt
@@ -0,0 +1,11 @@
+package teamfood.menufoodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Especias : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_especias)
+    }
+}
---
+++ app/src/main/java/teamfood/menufoodapp/Ingredientes.kt
@@ -0,0 +1,11 @@
+package teamfood.menufoodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ingredientes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingredientes)
+    }
+}
