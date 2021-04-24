6af08df7b1319ec84f168d559fc6986a3c820993
Brayan-Alexis <brayanzxd@gmail.com>
2021-04-23 20:00:33 -0700

    Funcionamiento de Login finishhhh

--- app/src/main/java/teamfood/menufoodapp/MainActivity.kt
+++ app/src/main/java/teamfood/menufoodapp/MainActivity.kt
@@ -1,27 +1,41 @@
 package teamfood.menufoodapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_login.*
+//import kotlinx.android.synthetic.main.activity_main.*
 
+
+const val RC_SIGN_IN = 343
+const val LOG_OUT = 234
 
 class MainActivity : AppCompatActivity() {
-    val RC_SIGN_IN = 123
+
     lateinit var mGoogleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        val sign_in_button: BottomNavigationView = findViewById(R.id.sign_in_button)
+
+        /*val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        //val signInButton: SignInButton = findViewById(R.id.sign_in_button)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
@@ -32,7 +46,7 @@ class MainActivity : AppCompatActivity() {
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navView.setupWithNavController(navController)*/
 
 
         // Configure sign-in to request the user's ID, email address, and basic
@@ -46,10 +60,62 @@ class MainActivity : AppCompatActivity() {
         // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-
         sign_in_button.setOnClickListener{
             val signInIntent = mGoogleSignInClient.signInIntent
             startActivityForResult(signInIntent, RC_SIGN_IN)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            val task =
+                GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+
+        }
+
+        if(requestCode == LOG_OUT){
+            signOut()
+        }
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this, OnCompleteListener<Void?> {
+                Toast.makeText(this,"Sesión terminada", Toast.LENGTH_SHORT).show()
+            })
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account =
+                completedTask.getResult(ApiException::class.java)
+
+            updateUI(account)
+        } catch (e: ApiException) {
+            Log.w("test_signin", "signInResult:failed code=" + e.statusCode)
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(acct: GoogleSignInAccount?) {
+
+        if (acct != null){
+            val intent = Intent(this, PrincipalActivity::class.java)
+            intent.putExtra("id", acct.getId())
+            intent.putExtra("name", acct.getDisplayName())
+            intent.putExtra("email", acct.getEmail())
+            startActivityForResult(intent, LOG_OUT)
         }
     }
 }
