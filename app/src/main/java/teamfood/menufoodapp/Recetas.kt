400c5162d91b12466d5590bb4712bd9236c7d10f
J. Eduardo Montoya S <33884031+Jemscabo98@users.noreply.github.com>
2021-04-24 17:51:03 -0600

    Fragment to Activity

---
+++ app/src/main/java/teamfood/menufoodapp/Recetas.kt
@@ -0,0 +1,11 @@
+package teamfood.menufoodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Recetas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recetas)
+    }
+}
