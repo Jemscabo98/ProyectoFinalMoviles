505809213bc34086189d9bb9a7f062457594cd08
J. Eduardo Montoya S <33884031+Jemscabo98@users.noreply.github.com>
2021-04-24 19:43:11 -0600

    Correcciones

--- app/src/main/java/teamfood/menufoodapp/Recetas.kt
+++ app/src/main/java/teamfood/menufoodapp/Recetas.kt
@@ -7,5 +7,6 @@ class Recetas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recetas)
+
     }
 }
