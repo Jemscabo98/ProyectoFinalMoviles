542bdadfff6306bffb9fede2d4b9cd4889b34d96
Brayan-Alexis <brayanzxd@gmail.com>
2021-04-28 01:41:35 -0700

    Minimo update

--- app/src/main/java/teamfood/menufoodapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/teamfood/menufoodapp/ui/dashboard/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package teamfood.menufoodapp.ui.dashboard
 
+import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,7 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import teamfood.menufoodapp.R
-
+import teamfood.menufoodapp.Recetas
 
 class DashboardFragment : Fragment() {
 
@@ -26,11 +27,18 @@ class DashboardFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
         val nombre_usuario: TextView = root.findViewById(R.id.text_dashboard)
+        val btnPizzaClasica: com.google.android.material.imageview.ShapeableImageView = root.findViewById(R.id.imgPizzaClasica)
 
 
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
 
+            btnPizzaClasica.setOnClickListener{
+                val intent: Intent = Intent(requireActivity(), Recetas::class.java)
+                startActivity(intent)
+            }
+
         })
+
         return root
     }
 }
