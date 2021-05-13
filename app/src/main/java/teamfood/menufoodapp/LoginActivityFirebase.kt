e122922e7333fd1763e3ec492d6a7bb54f6418b9
J. Eduardo Montoya S <33884031+Jemscabo98@users.noreply.github.com>
2021-05-12 22:18:36 -0600

    Agregaron imagenes

--- app/src/main/java/teamfood/menufoodapp/LoginActivityFirebase.kt
+++ app/src/main/java/teamfood/menufoodapp/LoginActivityFirebase.kt
@@ -57,24 +57,24 @@ class LoginActivityFirebase : AppCompatActivity() {
     }
 
     private fun ingresaFirebase(email: String, password: String){
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    //Log.d(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    //updateUI(user)
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                //Log.d(TAG, "signInWithEmail:success")
+                val user = auth.currentUser
+                //updateUI(user)
 
-                    val intent: Intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
+                val intent: Intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
 
-                } else {
-                    // If sign in fails, display a message to the user.
-                    //Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    //updateUI(null)
-                }
+            } else {
+                // If sign in fails, display a message to the user.
+                //Log.w(TAG, "signInWithEmail:failure", task.exception)
+                Toast.makeText(baseContext, "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+                //updateUI(null)
             }
+        }
     }
 }
