87a5dc47f7fe13b6542ec14f153c788bc11311ea
J. Eduardo Montoya S <33884031+Jemscabo98@users.noreply.github.com>
2021-05-24 11:35:21 -0600

    Agrega imagen de receta

--- app/src/main/java/teamfood/menufoodapp/RegistrarReceta.kt
+++ app/src/main/java/teamfood/menufoodapp/RegistrarReceta.kt
@@ -3,6 +3,7 @@ package teamfood.menufoodapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -22,10 +23,58 @@ class RegistrarReceta : AppCompatActivity() {
         val ingredientes: GridView = findViewById(R.id.gridRegistro)
         val pasos: EditText = findViewById(R.id.pasosRecetas)
         val btnRegistrar: Button = findViewById(R.id.btnRegistrarReceta)
+        val spinner: Spinner = findViewById(R.id.spinner)
 
         storage = FirebaseFirestore.getInstance()
         usuario = FirebaseAuth.getInstance()
 
+        //Conecciones al spinner
+        val lista: ArrayList<String> = SpinnerLista()
+        val adp = ArrayAdapter(this, R.layout.spinner_list, lista)
+        spinner.setAdapter(adp)
+        var imagen: Int = randomNum()
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                var elemento: String = spinner.adapter.getItem(position).toString()
+
+                if (elemento.equals("Ensalada 1")){
+                    imagenReceta.setImageResource(R.drawable.subidas_5)
+                    imagen = R.drawable.subidas_5
+                }
+
+                if (elemento.equals("Ensalada 2")){
+                    imagenReceta.setImageResource(R.drawable.subidas_6)
+                    imagen = R.drawable.subidas_6
+                }
+
+                if (elemento.equals("Comida 1")){
+                    imagenReceta.setImageResource(R.drawable.subidas_2)
+                    imagen = R.drawable.subidas_2
+                }
+
+                if (elemento.equals("Comida 2")){
+                    imagenReceta.setImageResource(R.drawable.subidas_3)
+                    imagen = R.drawable.subidas_3
+                }
+
+                if (elemento.equals("Postre 1")){
+                    imagenReceta.setImageResource(R.drawable.subidas_1)
+                    imagen = R.drawable.subidas_1
+                }
+
+                if (elemento.equals("Postre 2")){
+                    imagenReceta.setImageResource(R.drawable.subidas_4)
+                    imagen = R.drawable.subidas_4
+                }
+
+                if (elemento.equals("Imagen Receta")){
+                    imagen = randomNum()
+                    imagenReceta.setImageResource(imagen)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {   }
+        }
 
         btnRegistrar.setOnClickListener {
             var intent = Intent(this, Recetas::class.java)
@@ -34,7 +83,7 @@ class RegistrarReceta : AppCompatActivity() {
             val actividad = hashMapOf(
                     "email" to usuario.currentUser?.email.toString(),
                     "nombre" to nombreReceta.text.toString(),
-                    "imagen" to randomNum(),
+                    "imagen" to imagen,
                     "dificultad" to dificultLevel.rating,
                     "ingredientes" to randomIng(),
                     "pasos" to pasos.text.toString(),
@@ -51,6 +100,19 @@ class RegistrarReceta : AppCompatActivity() {
 
             startActivity(intent)
         }
+    }
+
+    fun SpinnerLista(): ArrayList<String>{
+        val lista :ArrayList<String> = ArrayList<String>()
+        lista.add("Imagen Receta")
+        lista.add("Ensalada 1")
+        lista.add("Ensalada 2")
+        lista.add("Comida 1")
+        lista.add("Comida 2")
+        lista.add("Postre 1")
+        lista.add("Postre 2")
+
+        return lista
     }
 
     fun randomNum(): Int {
