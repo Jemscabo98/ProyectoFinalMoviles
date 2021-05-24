66c415cd1ca9948bc335561fc1d0f294e21bab3f
J. Eduardo Montoya S <33884031+Jemscabo98@users.noreply.github.com>
2021-05-24 13:12:43 -0600

    Fix esteticos

--- app/src/main/java/teamfood/menufoodapp/Recetas_Grid.kt
+++ app/src/main/java/teamfood/menufoodapp/Recetas_Grid.kt
@@ -24,13 +24,13 @@ class Recetas : AppCompatActivity() {
         setContentView(R.layout.activity_recetas_grid)
 
         var gridRecetas: GridView = findViewById(R.id.gridRecetas)
-        var btnRegreso: Button = findViewById(R.id.btnVolver)
+        var btnRegreso: ImageView = findViewById(R.id.imgCategoria)
 
         btnRegreso.setOnClickListener {
             val intent: Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-        ///*
+
         recetas = ArrayList<Receta>()
         storage = FirebaseFirestore.getInstance()
         usuario = FirebaseAuth.getInstance()
@@ -44,64 +44,6 @@ class Recetas : AppCompatActivity() {
             adapter = RecetaAdapter(this, recetas)
             gridRecetas.adapter = adapter
         }
-        //*/
-
-        /*
-        val bundle = intent.extras
-        if(bundle != null){
-            if (bundle.getString("name") == "RecetasCompletas"){
-                cargarRecetasCompletas()
-                adapter = RecetaAdapter(this, recetas)
-                gridRecetas.adapter = adapter
-            }
-
-            if (bundle.getString("name") == "RecetasSubidas"){
-                cargarRecetasSubidas()
-
-                var nombre = bundle.getString("nombre")
-                var ingredientes = bundle.getDouble("dificultad")
-                var imagen = bundle.getDouble("imagen")
-                var pasos = bundle.getString("pasos")
-                var clasificacion = bundle.getString("clasificacion")
-
-                var args = bundle.getBundle("BUNDLE")
-                if (args != null) {
-                    var aux = args.getSerializable("ARRAYLIST") as ArrayList<Double>
-
-                    if (nombre != null && pasos!= null) {
-                        var receta: Receta = Receta(
-                            nombre,
-                            imagen,
-                            ingredientes,
-                            aux,
-                            pasos,
-                            clasificacion)
-
-                        recetas.add(receta)
-                    }else{
-                        var receta: Receta = Receta(
-                                "nombre",
-                                imagen,
-                                ingredientes,
-                                aux,
-                                "pasos",
-                                "RecetasSubidas")
-
-                        recetas.add(receta)
-                    }
-                }
-
-                adapter = RecetaAdapter(this, recetas)
-                gridRecetas.adapter = adapter
-            }
-
-            if (bundle.getString("name") == "RecetasFavoritas"){
-                cargarRecetasFavoritas()
-                adapter = RecetaAdapter(this, recetas)
-                gridRecetas.adapter = adapter
-            }
-        }
-        */
     }
 
     fun filltask(gridRecetas: GridView, bundle: Bundle){
@@ -149,18 +91,24 @@ class Recetas : AppCompatActivity() {
                 }
 
                 if (bundle.getString("name") == "RecetasCompletas"){
+                    val txt: TextView = findViewById(R.id.txtCategoriaReceta)
+                    txt.setText("Recetas Completas")
                     recetas = RecetasCompletas
                     adapter = RecetaAdapter(this, recetas)
                     gridRecetas.adapter = adapter
                 }
 
                 if (bundle.getString("name") == "RecetasSubidas"){
+                    val txt: TextView = findViewById(R.id.txtCategoriaReceta)
+                    txt.setText("Recetas Subidas")
                     recetas = RecetasSubidas
                     adapter = RecetaAdapter(this, recetas)
                     gridRecetas.adapter = adapter
                 }
 
                 if (bundle.getString("name") == "RecetasFavoritas"){
+                    val txt: TextView = findViewById(R.id.txtCategoriaReceta)
+                    txt.setText("Recetas Favoritas")
                     recetas = RecetasFavoritas
                     adapter = RecetaAdapter(this, recetas)
                     gridRecetas.adapter = adapter
@@ -169,71 +117,6 @@ class Recetas : AppCompatActivity() {
             .addOnFailureListener{
                 Toast.makeText(this, "Error: intente de nuevo", Toast.LENGTH_SHORT).show()
             }
-    }
-
-
-    fun cargarRecetasCompletas(){
-        var pizza = ArrayList<Double>()
-        pizza.add(R.drawable.icon_queso_ldpi.toDouble())
-        pizza.add(R.drawable.icon_sal_ldpi.toDouble())
-        pizza.add(R.drawable.icon_pan_ldpi.toDouble())
-        recetas.add(Receta("Pizza", R.drawable.completadas_5.toDouble(), 4.5, pizza,
-                "1. ¡Manos a la masa!\n " +
-                        "Mezcla la harina con la levadura, la sal y el aceite\n" +
-                        "Añade agua poco a poco hasta que obtengas una consistencia suave y todos los ingredientes estén bien incorporados\n" +
-                        "Haz una bola con la masa que preparaste y colócala dentro de un recipiente que previamente hayas enharinado\n" +
-                        "Espera 45 minutos a que la mezcla que hiciste aumente su tamaño al doble\n\n" +
-                        "2. Sin salsa no hay sabor\n" +
-                        "Sofríe ajo en una olla o sartén a temperatura baja para lograr que se dore por fuera, pero que suelte todo su sabor\n" +
-                        "Añade la salsa de tomate, aceite, comino, orégano, sal y azúcar\n" +
-                        "Deja que se cocine de 10 a 15 minutos\n" +
-                        "Retira y déjalo enfriar", "RecetasCompletas"))
-
-        var ensalada = ArrayList<Double>()
-        ensalada.add(R.drawable.icon_zanahoria_ldpi.toDouble())
-        ensalada.add(R.drawable.icon_aguacate_ldpi.toDouble())
-        ensalada.add(R.drawable.icon_espinaca_ldpi.toDouble())
-        ensalada.add(R.drawable.icon_brocoli_ldpi.toDouble())
-        recetas.add(Receta("Ensalada", R.drawable.completadas_6.toDouble(), 2.0, ensalada,
-                "1. Elegir una base con una variedad de hojas verdes y otros vegetales.\n" +
-                        "2. Agregar dos porciones de proteínas ya sea de origen animal o vegetal.\n" +
-                        "3. Endulzar con media taza de fruta.\n" +
-                        "4. Agregar una cucharada de frutos secos o cereales.\n" +
-                        "5. Una cucharada de algun tipo de queso.\n" +
-                        "6. Una cucharada de un aderezo.", "RecetasCompletas"))
-    }
-
-    fun cargarRecetasSubidas(){
-        var ensalada = ArrayList<Double>()
-        ensalada.add(R.drawable.icon_zanahoria_ldpi.toDouble())
-        ensalada.add(R.drawable.icon_aguacate_ldpi.toDouble())
-        ensalada.add(R.drawable.icon_espinaca_ldpi.toDouble())
-        ensalada.add(R.drawable.icon_brocoli_ldpi.toDouble())
-        recetas.add(Receta("Ensalada", R.drawable.completadas_6.toDouble(), 2.0, ensalada,
-            "1. Elegir una base con una variedad de hojas verdes y otros vegetales.\n" +
-                    "2. Agregar dos porciones de proteínas ya sea de origen animal o vegetal.\n" +
-                    "3. Endulzar con media taza de fruta.\n" +
-                    "4. Agregar una cucharada de frutos secos o cereales.\n" +
-                    "5. Una cucharada de algun tipo de queso.\n" +
-                    "6. Una cucharada de un aderezo.", "RecetasCompletas"))
-    }
-
-    fun cargarRecetasFavoritas(){
-        var pizza = ArrayList<Double>()
-        pizza.add(R.drawable.icon_queso_ldpi.toDouble())
-        pizza.add(R.drawable.icon_sal_ldpi.toDouble())
-        pizza.add(R.drawable.icon_pan_ldpi.toDouble())
-        recetas.add(Receta("Pizza", R.drawable.completadas_5.toDouble(), 4.5, pizza,
-            "1. ¡Manos a la masa!\n " +
-                    "Mezcla la harina con la levadura, la sal y el aceite\n" +
-                    "Añade agua poco a poco hasta que obtengas una consistencia suave y todos los ingredientes estén bien incorporados\n" +
-                    "Haz una bola con la masa que preparaste y colócala dentro de un recipiente que previamente hayas enharinado\n" +
-                    "Espera 45 minutos a que la mezcla que hiciste aumente su tamaño al doble\n\n" +
-                    "2. Sin salsa no hay sabor\n" +
-                    "Sofríe ajo en una olla o sartén a temperatura baja para lograr que se dore por fuera, pero que suelte todo su sabor\n" +
-                    "Añade la salsa de tomate, aceite, comino, orégano, sal y azúcar\n" +
-                    "Deja que se cocine de 10 a 15 minutos\n" +
-                    "Retira y déjalo enfriar", "RecetasCompletas"))
     }
 }
 
