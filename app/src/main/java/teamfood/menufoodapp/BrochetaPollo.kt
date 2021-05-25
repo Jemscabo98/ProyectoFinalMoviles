9e3cc009aba9958af6cf6b07f22fcf0798bb81d7
Brayan-Alexis <brayanzxd@gmail.com>
2021-05-25 08:50:35 -0700

    Actualizacion dashboard

---
+++ app/src/main/java/teamfood/menufoodapp/BrochetaPollo.kt
@@ -0,0 +1,11 @@
+package teamfood.menufoodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BrochetaPollo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_brocheta_pollo)
+    }
+}
--- app/src/main/java/teamfood/menufoodapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/teamfood/menufoodapp/ui/dashboard/DashboardFragment.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.android.synthetic.main.fragment_dashboard.*
 import teamfood.menufoodapp.Pizzaclasica
 import teamfood.menufoodapp.R
 import teamfood.menufoodapp.ui.home.RegistrarReceta
@@ -29,6 +29,9 @@ class DashboardFragment : Fragment() {
 
         val nombre_usuario: TextView = root.findViewById(R.id.text_dashboard)
         val btnPizzaClasica: com.google.android.material.imageview.ShapeableImageView = root.findViewById(R.id.imgPizzaClasica)
+        val btnChiken: com.google.android.material.imageview.ShapeableImageView = root.findViewById(R.id.imgChiken)
+        val btnCake: com.google.android.material.imageview.ShapeableImageView = root.findViewById(R.id.imgCake)
+        val btnRegistrarReceta: ImageView = root.findViewById(R.id.registrarReceta)
 
         dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
 
@@ -37,7 +40,17 @@ class DashboardFragment : Fragment() {
                 startActivity(intent)
             }
 
-            registrarReceta.setOnClickListener{
+            btnChiken.setOnClickListener{
+                val intent: Intent = Intent(requireActivity(), Pizzaclasica::class.java)
+                startActivity(intent)
+            }
+
+            btnCake.setOnClickListener{
+                val intent: Intent = Intent(requireActivity(), Pizzaclasica::class.java)
+                startActivity(intent)
+            }
+
+            btnRegistrarReceta.setOnClickListener{
                 val intent: Intent = Intent(requireActivity(), RegistrarReceta::class.java)
                 startActivity(intent)
             }
